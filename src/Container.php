e3379aa61b013ae47464d7a2318c345f08c7820e
Alexey Volkov <webwizardry@hotmail.com>
2024-09-12 16:02:51 +0300

    Mass

--- src/Container.php
+++ src/Container.php
@@ -3,7 +3,17 @@ declare(strict_types=1);
 
 namespace WebWizardry\Di;
 
+use Psr\Http\Message\RequestInterface;
+use WebWizardry\Config\Definition\Definitions\Reference;
+
 class Container extends \DI\Container
 {
-
+    public function get(string $id): object
+    {
+        $o = parent::get($id);
+        if ($o instanceof Reference) {
+            return $o->resolve($this);
+        }
+        return $o;
+    }
 }
--- src/ContainerBuilder.php
+++ src/ContainerBuilder.php
@@ -3,10 +3,19 @@ declare(strict_types=1);
 
 namespace WebWizardry\Di;
 
+use Psr\Container\ContainerInterface;
+
 class ContainerBuilder extends \DI\ContainerBuilder implements ContainerBuilderInterface
 {
     public function __construct(string $containerClass = Container::class)
     {
         parent::__construct($containerClass);
+    }
+
+    public function build(): ContainerInterface
+    {
+        $container = parent::build();
+        $container->set(ContainerInterface::class, $container);
+        return $container;
     }
 }
